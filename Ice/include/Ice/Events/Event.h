b986b392562fd7f87274405f2f6c4ea7a78ea8fe
icetd <tiandengzbc@gmail.com>
2023-03-06 21:58:27 +0800

    Update Events system

--- Ice/include/Ice/Events/Event.h
+++ Ice/include/Ice/Events/Event.h
@@ -26,7 +26,7 @@ namespace Ice {
 	};
 
 #define EVENT_CLASS_TYPE(type) \
-	static EventType GetStaticType() { return EventType::##type; } \
+	static EventType GetStaticType() { return EventType::type; } \
 	virtual EventType GetEventType() const override { return GetStaticType(); } \
 	virtual const char *GetName() const override { return #type; }
 
